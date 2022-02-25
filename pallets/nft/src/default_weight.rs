11970ef4d8b0c50ad5207ed05b092f381680c0c9
terry.tu <tuminfei1981@gmail.com>
2022-02-25 10:53:16 +0800

    add batch create item

--- pallets/nft/src/default_weight.rs
+++ pallets/nft/src/default_weight.rs
@@ -86,6 +86,11 @@ impl crate::WeightInfo for () {
 			.saturating_add(DbWeight::get().reads(5 as Weight))
 			.saturating_add(DbWeight::get().writes(3 as Weight))
 	}
+	fn batch_create_nft_item() -> Weight {
+		(66_234_000 as Weight)
+			.saturating_add(DbWeight::get().reads(5 as Weight))
+			.saturating_add(DbWeight::get().writes(3 as Weight))
+	}
 	fn unlock() -> Weight {
 		(66_234_000 as Weight)
 			.saturating_add(DbWeight::get().reads(5 as Weight))
